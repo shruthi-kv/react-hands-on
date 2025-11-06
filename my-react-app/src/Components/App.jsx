c3082abe0247ac6d911c0c8e234160f0663c3661
Shruthi KV <shruthikv@Mac.lan>
2025-11-06 22:30:42 +0530

    higher-1

--- my-react-app/src/Components/App.jsx
+++ my-react-app/src/Components/App.jsx
@@ -1,11 +1,13 @@
 import Container from "./Container";
 import {WelcomeComp} from "./Higher";
+import {EnhancedComponent} from './Logger'
 
 export default function App() {
   return (
     <>
       <Container />
-      <WelcomeComp name="Shruti" />
+      {/* <WelcomeComp name="Shruti" /> */}
+      <EnhancedComponent name="Hello"/>
     </>
   );
 }

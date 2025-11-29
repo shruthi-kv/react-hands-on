d15fb2bc8b8767d42abc2f8dc596cc1dab5693b6
Shruthi KV <shruthikv@Shruthis-MacBook-Air.local>
2025-11-29 20:15:28 +0530

    start rating

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,9 +1,19 @@
-import Password from './Practice/Password';
+import StarRating from './Practice/Star';
+import { useState } from "react";
+import Rating from './Practice/Rating'
 
 const App = () => {
   
+ const [rating, setRating] = useState(0);
+
   return (
-    <Password/>
+    <div>
+      <h2>Star Rating (Controlled)</h2>
+      <StarRating value={rating} onChange={setRating} />
+      <p>Selected rating: {rating}</p>
+      <hr/>
+      <Rating/>
+    </div>
   );
 };
 

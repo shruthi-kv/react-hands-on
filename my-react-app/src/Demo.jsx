d0df7beca89c5f41ea9b3d5f4cce3af2039f79ce
Shruthi KV <shruthikv@Shruthis-MacBook-Air.local>
2025-11-30 17:22:15 +0530

    timer- watch

--- my-react-app/src/Demo.jsx
+++ my-react-app/src/Demo.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import Todo from './HandsOn/Todo';
-import Check from './HandsOn/Check'
+import Check from './HandsOn/Check';
+import Time from './HandsOn/Time';
 
 const Demo = ()=>{
 
     return(
         <>
         {/* <Todo/> */}
-        <Check/>
+        {/* <Check/> */}
+        <Time/>
         </>
     )
 }
